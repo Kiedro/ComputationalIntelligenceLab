8bfda59971e1d3039934b3dca6cfb4aa8c90cecb
Maciej Kiedrowski <maciej.kiedrowski@synergycodes.com>
2017-05-04 14:02:15 +0200

    TSP problem

---
+++ src/TspProblem.R
@@ -0,0 +1,34 @@
+rm(list = ls())
+
+library(GA)
+library(TSP)
+
+# "global" variable used by everything
+tsp <- read_TSPLIB(system.file("examples/kroA100.tsp", package = "TSP"))
+
+# best possible
+optimalTour <- solve_TSP(tsp, method = "nn", two_opt = TRUE)
+plot(tsp, optimalTour, cex=.6, col = "red", pch= 3, main = "TSPLIB: kroA100")
+
+################ implementation ###############
+
+getFitness <- function(sequention, tspProblem){
+  tour <- TOUR(sequention, method = NA, tsp = tspProblem)
+  tour_length(tour)
+}
+
+GA <- ga(type = "permutation",                       # for optimization problems where the decision variables are ...
+         fitness =  function(x) -getFitness(x, tsp),   # fitness function - takes as input an individual string representing ...
+         # ... a potential solution, and returns a numerical value describing its fitness.
+         min = 1,
+         max = 100,
+         popSize = 50,                  # population size
+         maxiter = 1000,                # maximum number of iterations to run before the GA search is halted
+         run = 100                      # the number of consecutive generations without any improvement ...
+)
+
+###############################################
+
+result = TOUR(GA@solution, method = NA, tsp = tsp)
+plot(tsp, result, cex=.6, col = "red", pch= 3, main = "TSPLIB: kroA100")
+
---
+++ src/tspData/README.txt
@@ -0,0 +1,2 @@
+Example file must be copied to downloaded TSP library, e.g:
+c:\program files\R\R-3.3.3\library\TSP\examples
