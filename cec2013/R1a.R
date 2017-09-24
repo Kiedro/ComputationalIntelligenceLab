908fc4a0303f7813394c6d0bf2320ed6d560c502
Maciej Kiedrowski <kiedro@outlook.com>
2017-09-24 18:04:55 +0200

    no idea

--- cec2013/R1a.R
+++ cec2013/R1a.R
@@ -17,31 +17,33 @@ cecF <- function(x1, x2)
 
 x1 <- seq(-60, 10, by = 0.1)          # seq() - generate function args - vector of 201 values from -500 to 500 with step of 5 ...
 x2 <- seq(-20, 50, by = 0.1)          # ... like -500, -495, -490, ... , 495, 500; for x1 and x2 each
- # y  <- outer(x1, x2, Vectorize(cecF))  # calc function value ...
-# ... outer() - apply function Vectorize(cecF) to two arrays (x1, x2)
-# ... Vectorize(cecF) vectorizes action of function cecF - makes it able to accept x1 and x2 vectors as its arguments
-# draw 3D chart
-# :param: bl2gr.colors - vector with palette of colors
-# :param: phi, theta   - angles defining the viewing direction, azimuthal, colatitude respectively
- # persp3D(x1, x2, y, theta = 50, phi = 20, color.palette = bl2gr.colors)  
 
-# draw section:
-# filled.contour(x1, x2, y, color.palette = bl2gr.colors)
 
+customCrossover <- function(object, parents, ...){
+	parents <- object@population[parents,,drop = FALSE]
+  
+}
+
+myMutation <- function (x, i) 
+{
+  return(rnorm(n=1, m=0, sd=3))
+} 
 # execution of genetic algorythm to search for global minimum
 results <- 0
 iterations <- 0
 meanValues <- 0
 
-for(i in 1:5) {
+for(i in 1:1) {
   GA <- ga(type = "real-valued",                       # for optimization problems where the decision variables are floating-point representations of real numbers
-           fitness =  function(x) -cecF(x[1], x[2]),   # fitness function - takes as input an individual string representing a potential solution, and returns a numerical value describing its â€œfitnessâ€.
+           fitness =  function(x) -cecF(x[1], x[2]),   # fitness function - takes as input an individual string representing a potential solution, and returns a numerical value describing its â€ofitnessâ€?.
            min = c(-50, -50),                        # a vector of length equal to the decision variables providing the minimum of the search space
            max = c(50, 50),                          # like above but maximum; c() function combines arguments and creates vector from them
            popSize = 50,                               # population size
-           maxiter = 1000,                             # maximum number of iterations to run before the GA search is halted
-           run = 300                                   # the number of consecutive generations without any improvement in the best fitness value before the GA is stopped
-  )
+           maxiter = 10,                             # maximum number of iterations to run before the GA search is halted
+           run = 300                                          # the number of consecutive generations without any improvement in the best fitness value before the GA is stopped
+           ,crossover = customCrossover
+           ,mutation = myMutation
+           )
   results[i] <- GA@fitnessValue
   meanValues[i] <- GA@summary[,2]
   iterations[i] <- GA@iter
